61f155609a8756d2ae0747cd2b5dfb15de67196a
lymhyt <hayatiyt62@gmail.com>
2023-03-31 09:30:25 +0800

    function register done

--- index.js
+++ index.js
@@ -29,10 +29,24 @@ let matched =dbUsers.find(element =>{
         }
     }
     else {
-        return "password not found"
+        return "username not found"
     }  
+}
+
+function register (newusername, newpassword, newname, newemail){
+    //to do :check if username exist
+
+    dbUsers.push({
+        username: newusername,
+        password: newpassword,
+        name: newname,
+        email:newemail,
+    })
 }
 
 //call function (give parameter) try to ;ogin
 //login("yat","pass")
 console.log(login("autem","passwo"))
+//try register
+register("wan","word", "wan2","mail")
+console.log(login("wan","word"))
